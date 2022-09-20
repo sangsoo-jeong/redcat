098c8ef8ae0ebbf98669d17d9cd99ea72ae1deeb
sangsoo <sangsoo-horangi@horangi.com>
2022-09-20 11:43:35 +0800

    Will be set all of the POSIX's header file

---
+++ project/Crypto/essential.h
@@ -0,0 +1,9 @@
+#include <stdio.h>  // standard related to input/output
+#include <string.h> // strcpy
+#include <unistd.h> // getopt
+#include <stdlib.h> // exit
+#define TRUE (1)
+#define FALSE (0)
+#define NONE (-1)
+
+//#define PRINT(data) printf(data)
